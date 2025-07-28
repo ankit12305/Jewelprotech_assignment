138c27c004291e7a30d25fbf66af46ed088fb17f
ankit12305 <92436888+ankit12305@users.noreply.github.com>
2025-07-28 11:13:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ provider "aws" {
 
 # No other resources directly in main.tf; they are imported from other files.
 # You might place module calls here if you convert these resources into a module later.
+
+
+
+
+  backend "s3" {
+    bucket         = "terraform-state-file-myapp" # <--- IMPORTANT: Replace with a globally unique S3 bucket name
+    key            = "my-app/terraform.tfstate"                 # Path to your state file within the bucket
+    region         = "us-east-1"                                # Match your AWS_REGION
+    encrypt        = true                                       # Encrypts the state file at rest
+    dynamodb_table = "terraform-state-lock-table"   # <--- IMPORTANT: Replace with a unique DynamoDB table name for state locking
+  }
