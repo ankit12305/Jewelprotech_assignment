ef65931435cf6e51e9881b0ae5eeef87d437d617
ankit12305 <92436888+ankit12305@users.noreply.github.com>
2025-07-28 10:32:35 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -11,8 +11,18 @@ resource "aws_efs_file_system" "my_efs" {
   }
 }
 
-resource "aws_efs_mount_target" "efs_mount_target" {
+# EFS Mount Target for Private Subnet 1
+resource "aws_efs_mount_target" "efs_mount_target_1" {
   file_system_id  = aws_efs_file_system.my_efs.id
-  subnet_id       = aws_subnet.public.id
+  # Use your first private subnet
+  subnet_id       = aws_subnet.private_1.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+# EFS Mount Target for Private Subnet 2
+resource "aws_efs_mount_target" "efs_mount_target_2" {
+  file_system_id  = aws_efs_file_system.my_efs.id
+  # Use your second private subnet
+  subnet_id       = aws_subnet.private_2.id
   security_groups = [aws_security_group.efs_sg.id]
 }
