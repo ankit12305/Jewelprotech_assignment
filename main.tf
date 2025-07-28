b303706dc861a6fad327b4a28eac0c4b281adf2e
ankit12305 <92436888+ankit12305@users.noreply.github.com>
2025-07-28 11:15:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,12 @@ provider "aws" {
 
 
 
-
-  backend "s3" {
+terraform {
+backend "s3" {
     bucket         = "terraform-state-file-myapp" # <--- IMPORTANT: Replace with a globally unique S3 bucket name
     key            = "my-app/terraform.tfstate"                 # Path to your state file within the bucket
     region         = "us-east-1"                                # Match your AWS_REGION
     encrypt        = true                                       # Encrypts the state file at rest
     dynamodb_table = "terraform-state-lock-table"   # <--- IMPORTANT: Replace with a unique DynamoDB table name for state locking
   }
+}
