8f7c52a35bfa21a16d7a76267091cbdb75bdba3a
ankit12305 <92436888+ankit12305@users.noreply.github.com>
2025-07-28 10:33:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,9 +5,25 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
-  description = "The ID of the public subnet"
-  value       = aws_subnet.public.id
+
+output "public_subnet_id_1" {
+  description = "The ID of the first public subnet"
+  value       = aws_subnet.public_1.id
+}
+
+output "public_subnet_id_2" {
+  description = "The ID of the second public subnet"
+  value       = aws_subnet.public_2.id
+}
+
+output "private_subnet_id_1" {
+  description = "The ID of the first private subnet"
+  value       = aws_subnet.private_1.id
+}
+
+output "private_subnet_id_2" {
+  description = "The ID of the second private subnet"
+  value       = aws_subnet.private_2.id
 }
 
 output "ecs_cluster_name" {
